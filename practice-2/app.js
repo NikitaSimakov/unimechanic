3a2e47e77e76ae1005eab477214983d30c6aa088
NikitaSimakov <mishaaniskin@gmail.com>
2024-04-26 23:00:34 +0300

    second practice task done

--- practice-2/app.js
+++ practice-2/app.js
@@ -1,17 +1,39 @@
-
 const fetchPosts = async () => {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=50');
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
 
     const data = await response.json();
-    console.log(data);
+    renderTable(data);
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
   }
+};
+
+const renderTable = (data) => {
+  const tableBody = document.querySelector('#posts-table tbody');
+
+  tableBody.innerHTML = '';
+
+  data.forEach(post => {
+    const row = document.createElement('tr');
+    const cell1 = document.createElement('td');
+    cell1.textContent = post.id;
+    row.appendChild(cell1);
+    const cell2 = document.createElement('td');
+    cell2.textContent = post.title;
+    row.appendChild(cell2);
+    const cell3 = document.createElement('td');
+    cell3.textContent = post.body;
+    row.appendChild(cell3);
+    const cell4 = document.createElement('td');
+    cell4.textContent = post.userId;
+    row.appendChild(cell4);
+    tableBody.appendChild(row);
+  });
 };
 
 fetchPosts();
